89a9a23b6dbe162b650eacbe177a11be08d061d5
Jan <abrell@posteo.de>
2024-01-18 17:45:51 +0100

    bugfixes

--- run_dashboard.py
+++ run_dashboard.py
@@ -2,5 +2,5 @@ from dashboard import dashboard
 
 
 if __name__ == "__main__":
-    fn_results = "./data/results_neu.parquet"
+    fn_results = "./data/result_used.parquet"
     dashboard(fn_results)
