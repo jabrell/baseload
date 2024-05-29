3247cbfa73f4bec725d2ea31aadd9f5580e5987d
Jan <abrell@posteo.de>
2024-05-29 11:32:07 +0200

    main for no gams model version

--- main_no_gams.py
+++ main_no_gams.py
@@ -1,11 +1,27 @@
 import numpy as np
+import pandas as pd
 from model_no_gams import simulate_storage_need_by_years
 
-if __name__ == "__main__":
+
+def run_all_combinations(
+    countries: list[str] = ["DE", "FR", "GR"],
+    fn_out: str = "./results/results_no_gams.parquet",
+    fn_renewable="./data/renewables_ninja.parquet",
+    fn_demand="./data/renewables_with_load.parquet",
+) -> pd.DataFrame:
+    """Run model for all weather and demand year combinations
+
+    Args:
+        countries (list[str], optional): List of countries to run the model for. Defaults to ["DE", "FR", "GR"].
+        fn_out (str, optional): File to save the results to. Defaults to "./results/results_no_gams.parquet".
+        fn_renewable (str, optional): File with renewable data. Defaults to "./data/renewables_ninja.parquet".
+        fn_demand (str, optional): File with demand data. Defaults to "./data/renewables_with_load.parquet".
+
+    Returns:
+        pd.DataFrame: Results for all model runs
+    """
     fn_renewable = "./data/renewables_ninja.parquet"
     fn_demand = "./data/renewables_with_load.parquet"
-
-    countries = ["DE", "FR", "GR"]
     years_re = range(1980, 2020)
     years_dem = range(2015, 2024)
     steps = 10
@@ -19,4 +35,11 @@ if __name__ == "__main__":
         fn_renewable=fn_renewable,
         fn_demand=fn_demand,
     )
-    df_out.to_parquet("./results/results_no_gams.parquet")
+    if fn_out is not None:
+        df_out.to_parquet(fn_out)
+
+
+if __name__ == "__main__":
+    fn_renewable = "./data/renewables_ninja.parquet"
+    fn_demand = "./data/renewables_with_load.parquet"
+    run_all_combinations()
---
+++ model_no_gams/__init__.py
@@ -0,0 +1,8 @@
+from .model import (
+    calculate_storage_need,
+    optimize_storage_need,
+    simulate_storage_need,
+    simulate_storage_need_by_years,
+)
+
+from .utils import get_profiles_shares, get_data_by_years
